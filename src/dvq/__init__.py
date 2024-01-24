f6218104ef2a77da920fe0f9e40ef890b64a2a25
hssn-20 <69965978+hssn-20@users.noreply.github.com>
2024-01-24 13:15:01 +0000

    directory stuff

--- src/dvq/__init__.py
+++ src/dvq/__init__.py
@@ -1,7 +1,7 @@
-from .visual import plot_2d_sequences, plot_2d_comparison
-from .statistical import permutation_overlap_comparison, permutation_overlap_group
+from .visual import *
+from .statistical import *
 
 
 
 
-__version__ = "0.0.4"
+__version__ = "0.0.4.1"
