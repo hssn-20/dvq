5728ddcad3b57b6ebcf83d601b2d5901d71ab181
hssn-20 <69965978+hssn-20@users.noreply.github.com>
2024-01-24 12:59:55 +0000

    updating the version and making the functions visible

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+from .visual import plot_2d_sequences, plot_2d_comparison
+from .statistical import permutation_overlap_comparison, permutation_overlap_group
+
+
+__all__ = ["visual", "statistical"]
+
+__version__ = "0.0.2"
