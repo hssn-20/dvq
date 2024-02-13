813cf0cbbe4635ec71226338f07ca00509b676c1
hssn-20 <69965978+hssn-20@users.noreply.github.com>
2024-02-13 18:38:23 +0000

    super minor naming change

--- src/dvq/__init__.py
+++ src/dvq/__init__.py
@@ -4,4 +4,4 @@ from .statistical import *
 
 
 
-__version__ = "0.0.4.6"
+__version__ = "0.0.4.6.1"
--- src/dvq/statistical/__init__.py
+++ src/dvq/statistical/__init__.py
@@ -1,4 +1,4 @@
 from .kmer_representation import permutation_overlap_group, permutation_overlap_comparison
-from .wen_method import similarity_wen, moment_of_inertia
+from .wens_method import similarity_wen, moment_of_inertia
 
 __all__ = ['permutation_overlap_group', 'permutation_overlap_comparison', 'similarity_wen', 'moment_of_inertia']
