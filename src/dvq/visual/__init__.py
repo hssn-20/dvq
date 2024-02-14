79b9a9dbce7cca4e4dea675031a1ec6088538a60
hssn-20 <69965978+hssn-20@users.noreply.github.com>
2024-02-14 10:34:20 +0000

    Update __init__.py

--- src/dvq/visual/__init__.py
+++ src/dvq/visual/__init__.py
@@ -1,3 +1,3 @@
-from .representation_2d import plot_2d_comparison, plot_2d_sequences
+from .representation_2d import plot_2d_comparison, plot_2d_sequences, GenerateFCGR
 
 __all__ = ['plot_2d_comparison', 'plot_2d_sequences', 'GenerateFCGR']
